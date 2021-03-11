a7a48a8fbc5435bacd1e3d9b401bf03fd8d65796
akirsteina <akirsteina@users.noreply.github.com>
2021-03-11 21:10:13 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,6 @@ console.log(`Hello, my name is ${person.name}, I live in ${person.address}, and 
 
 const cars = ['Vaz', 'Zaz', 'Gaz', 'Uaz', 'Maz'];
 
-for (i = 4; i > -1; i--) {
+for (i = cars.length; i > 0; i--) {
     console.log(`Car number ${i+1} is: ${cars[i]}`)
 }
